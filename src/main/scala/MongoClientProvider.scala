f61d25538f876f3c25f611e99b7202f05f2a77ca
anna-skrodzka <annaskrod@h=gmail.com>
2025-06-09 14:40:24 +0200

    MongoClientProvider added

---
+++ src/main/scala/MongoClientProvider.scala
@@ -0,0 +1,7 @@
+import com.mongodb.client.{MongoClient, MongoClients, MongoCollection, MongoDatabase}
+import org.bson.Document
+
+object MongoClientProvider:
+  val client: MongoClient = MongoClients.create("mongodb://localhost:27017")
+  val database: MongoDatabase = client.getDatabase("angermess")
+  val messages: MongoCollection[Document] = database.getCollection("messages")
